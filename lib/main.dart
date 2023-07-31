4c121999a7a4fc79620c6c38f643fa8277575d93
01nency <nencypatel175@gmail.com>
2023-07-31 09:01:00 +0530

    fisrt commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_app/views/home_page.dart';
+import 'package:youtube_app/views/openpage.dart';
+import 'package:youtube_app/views/searchpage.dart';
+
+
+void main(){
+  runApp(MyApp(),);
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context)=>HomePage(),
+        'openPage':(context)=>PlayerPage(),
+        'searchPage':(context)=>SearchPage(),
+      },
+    );
+  }
+}
